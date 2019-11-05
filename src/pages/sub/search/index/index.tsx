8c20c121dc825f43eb07cf6427a7348be618caa6
余兴良 <yuxl@bnd.com>
2019-11-05 10:26:50 +0800

    修改

--- src/pages/sub/search/index/index.tsx
+++ src/pages/sub/search/index/index.tsx
@@ -1,16 +1,11 @@
 import Taro, { Component, Config } from "@tarojs/taro";
 import { View, Text, Button } from "@tarojs/components";
-import { observer, inject } from "@tarojs/mobx";
-import WishShare from "@decorators/wishShare";
 
 interface IProps {}
 
 const initialState = { clickCount: 0 };
 type State = Readonly<typeof initialState>;
 
-@inject("commonStore")
-@observer
-@WishShare()
 export default class Index extends Component<IProps, State> {
   state: State = initialState;
   // eslint-disable-next-line react/sort-comp
